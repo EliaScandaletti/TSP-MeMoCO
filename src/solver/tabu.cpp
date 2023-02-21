316f6af249832d22d5720e55c41bbfa02c864694
Elia Scandaletti <elia.scandaletti@gmail.com>
2023-02-21 13:45:48 +0100

    Fix bug + return # iter

--- src/solver/tabu.cpp
+++ src/solver/tabu.cpp
@@ -30,9 +30,9 @@ double evaluate_opt2(const Instance &tsp, const Path &currSol,
 
 bool next_opt2(const Instance &tsp, const Path &currSol, Path::opt2 &m,
                const std::deque<Path::opt2> &tabu, double acc_inc) {
-  double best_inc = 0;
+  double best_inc = -1e-6;
   bool found = false;
-  for (int i = 0; i < tsp.n() - 1; i++) {
+  for (int i = 1; i < tsp.n() - 1; i++) {
     for (int j = i + 1; j < tsp.n(); j++) {
       Path::opt2 mov = {i, j};
       double inc = evaluate_opt2(tsp, currSol, mov);
@@ -69,7 +69,7 @@ double evaluate_opt2_5(const Instance &tsp, const Path &currSol,
 }
 
 bool next_opt2_5(const Instance &tsp, const Path &currSol, Path::opt2_5 &m) {
-  double best_inc = 0;
+  double best_inc = -1e-6;
   bool found = false;
   for (int i = 0; i < tsp.n(); i++) {
     for (int j = 0; j < tsp.n(); j++) {
@@ -89,67 +89,62 @@ bool next_opt2_5(const Instance &tsp, const Path &currSol, Path::opt2_5 &m) {
   return found;
 };
 
-bool Tabu::solve(const Instance &tsp, Path &sol, size_t tabu_size,
-                 int max_iter) {
-  try {
-    bool stop = false;
-    int iter = 0;
+int Tabu::solve(const Instance &tsp, Path &sol, size_t tabu_size,
+                int max_iter) {
+  bool stop = false;
+  int iter = 0;
 
-    Path curr_sol = sol;
-    double best_val, curr_val;
-    best_val = curr_val = evaluate(tsp, curr_sol);
+  Path curr_sol = sol;
+  double best_val, curr_val;
+  best_val = curr_val = evaluate(tsp, curr_sol);
 
-    std::deque<Path::opt2> tabu_list;
+  std::deque<Path::opt2> tabu_list;
 
-    while (!stop && iter < max_iter) {
-      stop = true;
+  while (!stop && iter < max_iter) {
+    stop = true;
 
-      Path::opt2 m2;
-      bool does_move =
-          next_opt2(tsp, curr_sol, m2, tabu_list, best_val - curr_val);
-      while (does_move && iter < max_iter) {
+    Path::opt2 m2;
+    bool does_move =
+        next_opt2(tsp, curr_sol, m2, tabu_list, best_val - curr_val);
+    while (does_move && iter < max_iter) {
+      stop = false;
+      ++iter;
+
+      // apply the move
+      curr_val += evaluate_opt2(tsp, curr_sol, m2);
+      curr_sol.apply_opt2(m2);
+      // update tabu list
+      tabu_list.push_back(m2);
+      if (tabu_list.size() > tabu_size)
+        tabu_list.pop_front();
+      // check if curr_sol is the best so far
+      if (curr_val < best_val) {
+        best_val = curr_val;
+        sol = curr_sol;
+      }
+      // find next move
+      does_move = next_opt2(tsp, curr_sol, m2, tabu_list, best_val - curr_val);
+    }
+
+    // either there is a local minimum or max_iter reached
+    if (iter < max_iter) {
+      // uses 2.5-opt to move past the local minimum
+      Path::opt2_5 m2_5;
+      does_move = next_opt2_5(tsp, curr_sol, m2_5);
+
+      if (does_move) {
         stop = false;
         ++iter;
-
         // apply the move
-        curr_val += evaluate_opt2(tsp, curr_sol, m2);
-        curr_sol.apply_opt2(m2);
-        // update tabu list
-        tabu_list.push_back(m2);
-        if (tabu_list.size() > tabu_size)
-          tabu_list.pop_front();
+        curr_val += evaluate_opt2_5(tsp, curr_sol, m2_5);
+        curr_sol.apply_opt2_5(m2_5);
         // check if curr_sol is the best so far
         if (curr_val < best_val) {
           best_val = curr_val;
           sol = curr_sol;
         }
-        // find next move
-        does_move =
-            next_opt2(tsp, curr_sol, m2, tabu_list, best_val - curr_val);
-      }
-
-      // either there is a local minimum or max_iter reached
-      if (iter < max_iter) {
-        // uses 2.5-opt to move past the local minimum
-        Path::opt2_5 m2_5;
-        does_move = next_opt2_5(tsp, curr_sol, m2_5);
-
-        if (does_move) {
-          // apply the move
-          curr_val += evaluate_opt2_5(tsp, curr_sol, m2_5);
-          curr_sol.apply_opt2_5(m2_5);
-          // check if curr_sol is the best so far
-          if (curr_val < best_val) {
-            best_val = curr_val;
-            sol = curr_sol;
-          }
-        }
       }
     }
-
-  } catch (std::exception &e) {
-    std::cout << ">>>EXCEPTION: " << e.what() << std::endl;
-    return false;
   }
-  return true;
+  return iter;
 }
