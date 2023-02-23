e12366040d4b391fec99fa16da083a823852ba4a
Elia Scandaletti <elia.scandaletti@gmail.com>
2023-02-23 12:29:50 +0100

    Updated run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,9 @@ make > /dev/null
 cd ..
 
 echo "data\t\t\tn\tsol\t\ttime" > exact.out
+echo "data\t\t\tn\ttab\tsol\t\titer\ttime" > tabu.out
 for d in data/*.dat; do
     echo $d
     ./src/exact $d >> exact.out
+    ./src/tabu $d >> tabu.out
 done
