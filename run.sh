10498c5319c2d24a3014f9334a5b2e9ff8843b96
Elia Scandaletti <elia.scandaletti@gmail.com>
2023-04-05 11:09:37 +0200

    Improved output

--- run.sh
+++ run.sh
@@ -1,13 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 cd src/
 make > /dev/null
 cd ..
 
-echo "data\t\t\tn\tsol\t\ttime" > exact.out
-echo "data\t\t\tn\ttab\tsol\t\titer\ttime" > tabu.out
+echo -e "data\t\t\tn\tsol\t\ttime" > exact.out
+echo -e "data\t\t\tn\ttab\tsol\t\titer\ttime" > tabu.out
 for d in data/*.dat; do
-    echo $d
+    echo "./src/exact $d"
     ./src/exact $d >> exact.out
-    ./src/tabu $d >> tabu.out
+    for t in {0..20..2}; do
+        echo "./src/tabu $d $t 100000"
+        ./src/tabu $d $t 100000 >> tabu.out
+    done
 done
