a8cdc80b916ae2615f78ae6fa2b87614eec35696
Elia Scandaletti <elia.scandaletti@gmail.com>
2023-04-19 19:27:26 +0200

    Generation by rectangles

--- generate.py
+++ generate.py
@@ -2,18 +2,35 @@ import math
 import random
 
 
-def generate(file, n, grid_size):
+def generate(n, grid_size):
     grid = [[False for j in range(grid_size)] for i in range(grid_size)]
     points = []
     c = 0
     while c < n:
-        i = random.choice(range(grid_size))
-        j = random.choice(range(grid_size))
-        if not grid[i][j]:
-            grid[i][j] = True
-            points.append((i, j))
+        x1 = random.choice(range(grid_size))
+        y1 = random.choice(range(grid_size))
+        x2 = random.choice(range(grid_size))
+        y2 = random.choice(range(grid_size))
+        if not grid[x1][y1]:
+            grid[x1][y1] = True
+            points.append((x1, y1))
             c += 1
+        if not grid[x1][y2]:
+            grid[x1][y2] = True
+            points.append((x1, y2))
+            c += 1
+        if not grid[x2][y1]:
+            grid[x2][y1] = True
+            points.append((x2, y1))
+            c += 1
+        if not grid[x2][y2]:
+            grid[x2][y2] = True
+            points.append((x2, y2))
+            c += 1
+    # print("\n".join(["".join(["#" if e else "_" for e in r]) for r in grid]))
+    return points
 
+def store(file, points, n):
     with open(file, "w") as f:
         f.write(str(len(points))+"\n")
         for i in range(len(points)):
@@ -28,4 +45,5 @@ def generate(file, n, grid_size):
 
 if __name__ == "__main__":
     for n in range(10, 101, 5):
-        generate("data/tsp"+str(n)+".dat", n, int(n/2))
+        grid = generate(n, int(n/2))
+        store("data/tsp"+str(n)+".dat", grid, int(n/2))
